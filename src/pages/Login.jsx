550a356c1d6765b81aa97104cbb08e6eb3d85ada
Al-Ameen Olayiwola <alameenolayiwola22@gmail.com>
2024-01-02 19:38:25 -0600

    react8

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,12 +1,10 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import Footer from '../comps/Footer/Footer';
 import Header from '../comps/Header/Header';
 
 const Home = () => {
     return (
       <>
-      <Link to={"/Login"}></Link>
       <Header/>
      <div className="Login">
       <h1>Login</h1>
