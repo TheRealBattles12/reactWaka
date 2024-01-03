ff83ed0aa8b4508cccd7b13f3a5b4fb0de45aa85
Al-Ameen Olayiwola <alameenolayiwola22@gmail.com>
2024-01-02 19:27:25 -0600

    react8

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -6,6 +6,7 @@ import Header from '../comps/Header/Header';
 const Home = () => {
     return (
       <>
+      <Link to={"/"}></Link>
       <Header/>
      <div className="Login">
       <h1>Login</h1>
