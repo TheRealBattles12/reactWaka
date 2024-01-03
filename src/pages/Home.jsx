cd6c9c392e34f2f874d94db2a4c403b67640b3c0
Al-Ameen Olayiwola <alameenolayiwola22@gmail.com>
2024-01-02 19:34:28 -0600

    react8

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Login } from 'react-router-dom';
 import Footer from '../comps/Footer/Footer';
 import Header from '../comps/Header/Header';
 
