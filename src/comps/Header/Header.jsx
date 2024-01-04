e4ec70c21b1ea29b2a29f6640807ad3db693597b
Al-Ameen Olayiwola <alameenolayiwola22@gmail.com>
2024-01-04 16:44:27 -0600

    react8

--- src/comps/Header/Header.jsx
+++ src/comps/Header/Header.jsx
@@ -1,37 +1,27 @@
-import React from 'react'
-import logo from "../../assets/images/logo.png"
-import './Header.css'
-import { Link } from 'react-router-dom'
-import Menu from './pages/Menu'
-import Home from './pages/Home'
-import Login from "./pages/Login"
-import Signup from "./pages/Signup"
+import React from 'react';
+import logo from "../../assets/images/logo.png";
+import './Header.css';
+import { Link } from 'react-router-dom';
+
 const Header = () => {
   return (
-  <div>
-    <div className='center-image'>
+    <div>
+      <div className='center-image'>
         <img src={logo} alt="" />
-        <br/>
-    </div>  
-        <div className='center'>   
-        <a href="/">Home</a>
-        
-        <a href="/Menu">Menus</a>
-        
-        <a href="/Login">Order Online</a>
-        
-        <a href="/Login">Reservations</a>
-        
-        <a href="/Login">Forum</a>
-        
-        <a href="/Login">Members</a>
-        
-        <a href="/Login">Book Online</a>
-        
-        <a href="/Login">Log-In</a>
-        </div>
-  </div> 
-  )
-}
+        <br />
+      </div>
+      <div className='center'>
+        <Link to="/">Home</Link>
+        <Link to="/Menu">Menus</Link>
+        <Link to="/Login">Order Online</Link>
+        <Link to="/Login">Reservations</Link>
+        <Link to="/Login">Forum</Link>
+        <Link to="/Login">Members</Link>
+        <Link to="/Login">Book Online</Link>
+        <Link to="/Login">Log-In</Link>
+      </div>
+    </div>
+  );
+};
 
-export default Header
+export default Header;
