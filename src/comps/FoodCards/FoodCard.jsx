b977832d1115adec15dbeb4fc3cfdc6a11566734
Al-Ameen Olayiwola <alameenolayiwola22@gmail.com>
2024-01-06 08:50:03 -0600

    Initial commit

--- src/comps/FoodCards/FoodCard.jsx
+++ src/comps/FoodCards/FoodCard.jsx
@@ -3,22 +3,15 @@ import { add, subtract, updateResult } from './Menu';
 import "./FoodCard.css"
 
 const Foodcard = (props) => {
-  const subtract = (value) => {
-    // Define your subtract logic here
-  };
-
-  const add = (value) => {
-    // Define your add logic here
-  };
-
-  return (
+  
+return (
     <div className="item">
       <div>
         <h3>{props.meal}</h3>
         <h4>{props.price}</h4>
           <div className="button">
           <button onClick={() => subtract(1, number)} id="minus-button">-</button>
-        <h1 id="Number-on-Cart1">{Number(0)}</h1>
+        <h1 id="Number-on-Cart1">0</h1>
         <button onClick={() => add(1, number)} id="plus-button">+</button>
           </div>
       </div>
--- src/comps/FoodCards/Menu.js
+++ src/comps/FoodCards/Menu.js
@@ -1,18 +1,20 @@
 // example.js
 
 export function updateResult(item, number) {
-    document.getElementById(`Number-on-Cart${item}`).textContent = number[item - 1];
+    document.getElementById(`Number-on-Cart${item}`).textContent = number;
   }
   
   export function add(item, number) {
-    number[item - 1] += 1;
+    number += 1;
     updateResult(item, number);
+    return number; // Return updated number value
   }
   
   export function subtract(item, number) {
-    if (number[item - 1] > 0) {
-      number[item - 1] -= 0;
+    if (number > 0) {
+      number -= 1;
+      updateResult(item, number);
     }
-    updateResult(item, number);
+    return number; // Return updated number value
   }
   
