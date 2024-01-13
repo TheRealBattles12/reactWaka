f8a6f7d4c5761d88aa40405f62ec9142d6d3c5b1
Al-Ameen Olayiwola <alameenolayiwola22@gmail.com>
2024-01-13 12:25:11 -0600

    Initial comet

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -9,6 +9,25 @@ const Signup = () => {
     <Link to={"/Signup"}></Link>
     <Header/>
     <div className="signup">
+    <h1>Signup</h1>
+        <div className="Username">
+        <h3>Username</h3>
+        <input type="text" id="username" name="username" required></input>
+        </div>
+        <br/>
+       <div className="Password">
+       <h3>Password</h3>
+       <input type="text" id="password" name="password" required></input>
+       </div> 
+       <div className="RepeatPassword">
+       <h3>Repeat Password</h3>
+       <input type="text" id="password" name="password" required></input>
+       </div> 
+       <button type="submit">Signup</button>
+       <div class="login-option">
+            <p>Already have an account?</p>
+            <a href="/Login">Log In</a>
+        </div>
     </div>
     <Footer/>
     </>  
