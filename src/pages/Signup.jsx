fefb7a88031f0427db36298b0ee4c8e2f03bca99
Al-Ameen Olayiwola <alameenolayiwola22@gmail.com>
2024-02-11 08:44:59 -0600

    First Commit

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,9 +1,31 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, { useState } from 'react'
+import { Await, Link } from 'react-router-dom'
 import Footer from '../comps/Footer/Footer'
 import Header from '../comps/Header/Header'
+import { transformWithEsbuild } from 'vite'
 
 const Signup = () => {
+  const [emails, setEmail] = useState("")
+  const [passwords, setPassword] = useState("")
+  
+  async function handleSubmit(event)  {
+      try {
+        const response = await fetch("localhost:3000/api/user/register", {
+              method:"post", 
+              headers:{
+                "Content-Type":"application/json"
+              },
+              body:JSON.stringify({
+                email: emails, 
+                password: passwords
+              })
+        })
+        const data = await response.json()
+        console.log(data)
+      } catch (error) {
+        console.log("Failed to call API")
+      }
+ }
   return (
     <>
     <Link to={"/Signup"}></Link>
@@ -23,7 +45,7 @@ const Signup = () => {
        <h3>Repeat Password</h3>
        <input type="text" id="password" name="password" required></input>
        </div> 
-       <button type="submit">Signup</button>
+       <button onClick={handleSubmit} type="submit">Signup</button>
     </div>
     <Footer/>
     </>  
