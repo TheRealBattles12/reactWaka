5fc7cbe027432e0b1bb4a2e6851edfec672550bf
Al-Ameen Olayiwola <alameenolayiwola22@gmail.com>
2024-02-19 14:11:59 -0600

    First Commit

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -4,13 +4,19 @@ import Footer from '../comps/Footer/Footer'
 import Header from '../comps/Header/Header'
 
 const Signup = () => {
-  const [emails, setEmail] = useState("Ben10@example.com")
-  const [passwords, setPassword] = useState("123")
-  const [names, setName] = useState("John")
+  const [emails, setEmail] = useState("")
+  const [passwords, setPassword] = useState("")
+  const [repeat, setRepeat] = useState("")
+  const [names, setName] = useState("")
+  const [errors, setError] = useState("")
   
   
   async function handleSubmit(event)  {
-      try {
+    if (passwords != repeat) {
+      setError("Oops, the passwords do not match!")
+    return 0
+  }
+    try {
         const response = await fetch("http://127.0.0.1:3000/api/user/register", {
               method:"post", 
               headers:{
@@ -22,6 +28,7 @@ const Signup = () => {
                 name: names
               })
         })
+        
         const data = await response.json()
         console.log(data)
       } catch (error) {
@@ -35,18 +42,33 @@ const Signup = () => {
     <div className="signup">
     <h1>Signup</h1>
         <div className="Mobile/Email">
-        <h3>Username</h3>
-        <input type="text" id="username" name="username" required></input>
+        <h3>Email</h3>
+        <input type="text" id="email" name="email" required value={emails}
+          onChange={(event)=> {setEmail(event.target.value)}}
+        />
+        </div>
+        <div className="Name">
+        <h3>Name</h3>
+        <input type="text" id="name" name="name" required value={names}
+          onChange={(event)=> {setName(event.target.value)}}
+        />
         </div>
         <br/>
        <div className="Password">
        <h3>Password</h3>
-       <input type="text" id="password" name="password" required></input>
+         <input type="text" id="password" name="password" required value={passwords}
+          onChange={(event)=> {setPassword(event.target.value)}}
+        />
        </div> 
        <div className="RepeatPassword">
        <h3>Repeat Password</h3>
-       <input type="text" id="password" name="password" required></input>
+         <input type="text" id="repeat" name="repeat" required value={repeat}
+          onChange={(event)=> {setRepeat(event.target.value)}}
+        />
        </div> 
+       <br/>
+       {errors && <p> {errors}
+        </p>}
        <button onClick={handleSubmit} type="submit">Signup</button>
     </div>
     <Footer/>
