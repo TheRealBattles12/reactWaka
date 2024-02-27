3c6b6477d804e52c281483deadddbb1590b2ed03
Al-Ameen Olayiwola <alameenolayiwola22@gmail.com>
2024-02-26 19:29:11 -0600

    Adding Scrolling Effect to the Navigation Bar

--- src/comps/Header/Header.jsx
+++ src/comps/Header/Header.jsx
@@ -6,6 +6,7 @@ import { Link } from 'react-router-dom';
 const Header = () => {
   return (
     <div className="naviGation">
+      <marquee behavior="smooth" direction="left">Welcome to GoWaka, the home of authentic Nigerian cusisine!</marquee>
       <div className='center-image'>
         <img src={logo} alt="" />
         <br />
